e19cc857fc906e1abf6e12a823e2c9fa0c989677
Rimon7765 <mazidxx1@gmail.com>
2025-03-29 11:46:10 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -7,7 +7,7 @@ module.exports = {
 	config: {
 		name: "owner",
 		aliases: ["info"],
-		author: "Hasan",
+		author: "Rimon",
 		role: 0,
 		shortDescription: " ",
 		longDescription: "",
@@ -18,19 +18,19 @@ module.exports = {
 	onStart: async function ({ api, event }) {
 		try {
 			const ownerInfo = {
-				name: '𝑯𝑨𝑺𝑨𝑵 𝑴𝑨𝑯𝑴𝑼𝑫',
+				name: 'YoUr RiMOn❤️‍🩹',
 				class: '𝑺𝑺𝑪 𝑪𝑨𝑵𝑫𝑰𝑫𝑨𝑻𝑬',
 				group: '𝑺𝑪𝑰𝑬𝑵𝑪𝑬',
 				gender: '𝑴𝑨𝑳𝑬',
-				Birthday: '𝟐𝟐-𝟎𝟕-𝟐𝟎𝟎𝟖',
+				Birthday: '𝟐0-𝟎8-𝟐𝟎𝟎𝟖',
 				religion: '𝑰𝑺𝑳𝑨𝑴',
-				hobby: '𝑭𝒍𝒊𝒓𝒕𝒊𝒏𝒈 😁',
-				Fb: 'https://www.facebook.com/Itz.HaSaN.00',
+				hobby: 'nothing',
+				Fb: 'https://www.facebook.com/it.z.rimon.216225',
 				Relationship: '𝑨𝑳𝑾𝑨𝒀𝑺 𝑩𝑬 𝑺𝑰𝑵𝑮𝑳𝑬',
-				Height: '5"4'
+				Height: '6'
 			};
 
-			const bold = 'https://i.imgur.com/Ch8ZvsM.mp4';
+			const bold = 'https://i.imgur.com/CNpkoYi.mp4';
 			const tmpFolderPath = path.join(__dirname, 'tmp');
 
 			if (!fs.existsSync(tmpFolderPath)) {
